c592f3a9f0759a90f2760599824d5771a3a1ef98
NoshinIslam <noshinislam16@gmail.com>
2025-02-22 20:47:02 +0600

    Included a while loop and used ANSI escape code.

--- main.c
+++ main.c
@@ -1,28 +1,37 @@
 #include<stdio.h>
+#include<windows.h>
 int main()
 {
     // position of A (spaceship) and | (bullet):
 
     int rowA=15,colA=15;
+    int rowB=rowA-1, colB=colA;
 
 
     //i used ANSI code to specify the location of A. A is the spaceship.
     printf("\033[%d;%dH",rowA,colA);
     printf("A");
 
-
-     int rowB=14,colB=15;
+    int duration=2;
     char E; //E for 'enter'. Assigning 'Enter' as to trigger bullets. '|' is a bullet.
     scanf("%c",&E);
-    printf("\033[%d;%dH",rowB,colB);
-    printf("|");
 
-    int i;
-    for(i=0;i<4;i++)
+    while(rowB>0)
     {
-            printf("\n");
+        printf("\033[%d;%dH",rowB,colB);
+        printf("|\n");
+        Sleep(duration*100);
 
-    }  
-    
+        printf("\033[%d;%dH",rowB,colB);
+        printf(" "); //adds a new line to eleminate buffering
+
+        rowB--; //moves the bullet up by one row
+
+    }
+    printf("\033[%d;%dH", rowA + 1, 1);
+
+
+
+
     return 0;
 }

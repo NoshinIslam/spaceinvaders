640c783619886232aa2879b24d39d631bffc5327
Noshin Islam <noshinislam16@gmail.com>
2025-04-16 03:55:38 +0600

    Included aliens and random alien movement functions

--- main.c
+++ main.c
@@ -3,8 +3,12 @@
 #define SCREEN_WIDTH 30
 #define SCREEN_HEIGHT 20
 
+
+
 int spaceshipRow = 18, spaceshipCol = 15; // spaceship position
 int bulletRow = -1, bulletCol = -1; //bullet position
+int aliens[MAX_ALIENS][2];
+
 
 void displaySpaceship() {
     printf("\033[%d;%dH", spaceshipRow, spaceshipCol);
@@ -31,8 +35,42 @@ void moveSpaceship(char key) {
 
 
 void shootBullet() {
-    if (bulletRow == -1) { // Only one bullet can exist at a time
+    if (bulletRow == -1) { // only one bullet can exist at a time
         bulletRow = spaceshipRow - 1;
         bulletCol = spaceshipCol;
+    }
+}
+
+
+void spawnAliens() {
+    for (int i = 0; i < MAX_ALIENS; i++) {
+        if (aliens[i][0] == -1) { // if an alien is not active
+            aliens[i][0] = 1;                // spawn at the top row
+            aliens[i][1] = 1 + rand() % 30;  // random columnn
+        }
+    }
+}
+
+void moveAliens() {
+    for (int i = 0; i < MAX_ALIENS; i++) {
+        if (aliens[i][0] != -1) { // if an alien is active
+            aliens[i][0]++; // moves downward
+            if (aliens[i][0] > SCREEN_HEIGHT) { // if the alien goes off the screen
+                aliens[i][0] = -1; // deactivates the alien
+                aliens[i][1] = -1;
+            }
+        }
+    }
+
+    // Randomly spawn new aliens
+    if (rand() % 10 < 3) { // 30% chance to spawn a new alien
+        spawnAliens();
+    }
+}
+void displayAliens() {
+    for (int i = 0; i < MAX_ALIENS; i++) {
+        if (aliens[i][0] != -1) { // if an alien is active
+            printf("\033[%d;%dHV\n", aliens[i][0], aliens[i][1]);
+        }
     }
 }

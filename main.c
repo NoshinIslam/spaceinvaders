88a857b86fa1b1329f50a4cc3a98a24b802302c9
Noshin Islam <noshinislam16@gmail.com>
2025-04-16 03:31:48 +0600

    Added spaceship position, movement and bullet function

--- main.c
+++ main.c
@@ -1,6 +1,38 @@
-#include<stdio.h>
-int main()
-{
-    printf("\nHello world!\n");
-    return 0;
+
+#include <stdio.h>
+#define SCREEN_WIDTH 30
+#define SCREEN_HEIGHT 20
+
+int spaceshipRow = 18, spaceshipCol = 15; // spaceship position
+int bulletRow = -1, bulletCol = -1; //bullet position
+
+void displaySpaceship() {
+    printf("\033[%d;%dH", spaceshipRow, spaceshipCol);
+    printf("A");
+}
+
+
+void moveSpaceship(char key) {
+    switch (key) {
+        case 'A': case 'a': // moves left
+            if (spaceshipCol > 1) spaceshipCol--;
+            break;
+        case 'D': case 'd': // moves right
+            if (spaceshipCol < SCREEN_WIDTH) spaceshipCol++;
+            break;
+        case 'W': case 'w': // moves up
+            if (spaceshipRow > 1) spaceshipRow--;
+            break;
+        case 'S': case 's': // moves down
+            if (spaceshipRow < SCREEN_HEIGHT) spaceshipRow++;
+            break;
+    }
+}
+
+
+void shootBullet() {
+    if (bulletRow == -1) { // Only one bullet can exist at a time
+        bulletRow = spaceshipRow - 1;
+        bulletCol = spaceshipCol;
+    }
 }

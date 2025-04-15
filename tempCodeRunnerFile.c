69d0683718e1ddcb5f7fb3dab307c9331605bb6f
NoshinIslam <noshinislam16@gmail.com>
2025-04-16 02:10:38 +0600

    TESTING

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    printf("\nHello world!\n");
+    printf("\nThis is a new day\n");
+    for(int i=0;i<3;i++)
+    {
+        printf("This is supposed to print 3 times.\n");
+    }
+    return 0;
+}
